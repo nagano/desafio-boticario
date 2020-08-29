97720f9996c5b1001fbe2457198b7ffc138cc6d0
nagano <michel.nagano@gmail.com>
2020-08-29 00:59:44 -0300

    Add shared global style

---
+++ src/app/shared/constants/layout.constants.ts
@@ -0,0 +1,9 @@
+export enum COLOR {
+    BLUE_1 = 'blue-1',
+    BLUE_2 = 'blue-2',
+    GREEN_1 = 'green-1',
+    GREEN_2 = 'green-2',
+    ORANGE_1 = 'orange-1',
+    ORANGE_2 = 'orange-2',
+    WHITE = 'white'
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './app/App';
 import './reset.scss';
+import './app/shared/style/global.scss';
 
 ReactDOM.render(
     <React.StrictMode>
