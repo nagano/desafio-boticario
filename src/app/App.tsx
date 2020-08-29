8864ee086897afb6d9825cb4377e400b042875ca
nagano <michel.nagano@gmail.com>
2020-08-29 14:13:50 -0300

    Add notification structure and service

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from 'history'
+import { ToastContainer } from 'react-toastify';
 
 // Routes
 import { MainRoutes } from './routes/main.routes';
+// Styles
+import 'react-toastify/dist/ReactToastify.min.css';
 
 // Shared history
 export const history = createBrowserHistory();
 
 const App = (): JSX.Element => {
     return (
-        <Router history={history}>
-            <Route component={MainRoutes} />
-        </Router>
+        <React.Fragment>
+            <Router history={history}>
+                <Route component={MainRoutes} />
+            </Router>
+
+            <ToastContainer />
+        </React.Fragment>
     );
 };
 
---
+++ src/app/shared/services/notification.service.ts
@@ -0,0 +1,58 @@
+import { toast, ToastOptions } from 'react-toastify';
+
+const defaultOptions: ToastOptions = {
+    closeOnClick: true,
+    autoClose: 3000,
+    position: 'top-right',
+    hideProgressBar: false,
+    pauseOnFocusLoss: false,
+    closeButton: false
+};
+
+export enum NOTIFICATION_ID {
+    OPERATION_IN_PROGRESS = 'OPERATION_IN_PROGRESS'
+};
+
+export default {
+
+    success(message: string|JSX.Element) {
+        toast(
+            message,
+            {
+                ...defaultOptions,
+                type: 'success',
+                className: 'toast-notification__success'
+            }
+        );
+    },
+
+    /**
+     * Show an error notification.
+     * If an id is passed, only 1 notification with the given id will be visible at a time.
+     * @param {string|JSX.Element} message 
+     * @param {NOTIFICATION_ID} id 
+     */
+    error(message: string|JSX.Element, id?: NOTIFICATION_ID) {
+        toast(
+            message,
+            {
+                ...defaultOptions,
+                toastId: id,
+                type: 'error',
+                className: 'toast-notification__error'
+            }
+        );
+    },
+
+    info(message: string|JSX.Element) {
+        toast(
+            message,
+            {
+                ...defaultOptions,
+                type: 'info',
+                className: 'toast-notification__info'
+            }
+        )
+    }
+
+}
