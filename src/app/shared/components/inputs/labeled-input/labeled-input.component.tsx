11ad941b48e3008aeff47faeeaeb0ee07bd5f10a
nagano <michel.nagano@gmail.com>
2020-08-30 00:29:30 -0300

    Update labeled-input to  show masked and password inputs

--- src/app/shared/components/inputs/labeled-input/labeled-input.component.tsx
+++ src/app/shared/components/inputs/labeled-input/labeled-input.component.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// Components
+import { MaskedInput } from '../masked-input/masked-input.component';
+import { PasswordInput } from '../password-input/password-input.component';
+
 interface LabeledInputProps {
     containerClass?: string;
     inputClass?: string;
@@ -7,21 +11,44 @@ interface LabeledInputProps {
     type?: string;
     label: string;
     value: string|number;
+    mask?: Array<string|RegExp>;
     onChange: (updatedValue: string|number) => void;
-    onKeyDown?: () => void;
+    onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 };
 
 export const LabeledInput = (props: LabeledInputProps): JSX.Element => {
+    // Default values
+    const type: string = props.type || 'text';
+
     return (
         <main className={`flexbox flex-column ${props.containerClass}`}>
             <label className="fz-label text--gray-4 mb-5" htmlFor={props.id}>{props.label}</label>
-            <input 
-                className={props.inputClass}
-                id={props.id}
-                type={props.type || 'text'}
-                value={props.value}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => props.onChange(event.target.value)}
-                onKeyDown={props.onKeyDown} />
+
+            {
+                props.mask ?
+                    <MaskedInput
+                        className={props.inputClass}
+                        id={props.id}
+                        mask={props.mask}
+                        value={props.value as string}
+                        onChange={(updatedValue: string) => props.onChange(updatedValue)} />
+                    :
+                    type === 'password' ?
+                        <PasswordInput
+                            className={props.inputClass}
+                            id={props.id}
+                            value={props.value as string}
+                            onChange={(updatedValue: string) => props.onChange(updatedValue)}
+                            onKeyDown={props.onKeyDown} />
+                        :
+                        <input 
+                            className={props.inputClass}
+                            id={props.id}
+                            type={type}
+                            value={props.value}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => props.onChange(event.target.value)}
+                            onKeyDown={props.onKeyDown} />
+            }
         </main>
     );
 };
