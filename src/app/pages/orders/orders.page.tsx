6174dca66fbc603ce91fae14341efd76bb288453
nagano <michel.nagano@gmail.com>
2020-08-30 01:23:13 -0300

    Add initial orders page

---
+++ src/app/pages/orders/orders.page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Style
+import './orders.page.scss';
+
+const OrdersPage = (): JSX.Element => {
+    return (
+        <div className="orders">
+            ORDERS
+        </div>
+    );
+};
+
+export default OrdersPage;
